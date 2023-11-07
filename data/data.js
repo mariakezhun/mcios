8fabb3ff5c302dbd0c6308ad67ad63bce3f33a9b
Maria Kezhun <marusa.kezhoon@yandex.ru>
2023-11-07 14:36:36 +0300

    add years and blocks, fix js

--- data/data.js
+++ data/data.js
@@ -5,9 +5,9 @@ const accordionItem = document.querySelector(
   '.data__accordion-item-header_type_white'
 );
 // const blueContainer = document.querySelector('.plus-height');
-const height = document.querySelector('.data__accordion-item-body.data__accordion-item-body_type_blue');
+const height = document.querySelectorAll('.data__accordion-item-body.data__accordion-item-body_type_blue');
 // console.log(height.offsetHeight)
-// console.log(height.style)
+console.log(height)
 
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener('click', (event) => {
@@ -22,8 +22,8 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     accordionItemHeader.classList.toggle('active');
     const accordionItemBody = accordionItemHeader.nextElementSibling;
     if (accordionItemHeader.classList.contains('active')) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + height.scrollHeight + 'px';
-      // console.log(height.scrollHeight)
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + height[0].scrollHeight + height[1].scrollHeight + 'px';
+      console.log(height[1].scrollHeight)
       // console.log(accordionItemHeader)
     } else {
       accordionItemBody.style.maxHeight = 0;
