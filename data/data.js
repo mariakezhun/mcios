3e118489954755a1d84f80b4ba0b707afd7b8b5a
Maria Kezhun <marusa.kezhoon@yandex.ru>
2023-11-21 16:23:37 +0300

    21.11.23 2

--- data/data.js
+++ data/data.js
@@ -5,9 +5,11 @@ const accordionItem = document.querySelector(
   '.data__accordion-item-header_type_white'
 );
 // const blueContainer = document.querySelector('.plus-height');
-const height = document.querySelectorAll('.data__accordion-item-body.data__accordion-item-body_type_blue');
+const height = document.querySelectorAll(
+  '.data__accordion-item-body.data__accordion-item-body_type_blue'
+);
 // console.log(height.offsetHeight)
-console.log(height)
+console.log(height);
 
 accordionItemHeaders.forEach((accordionItemHeader) => {
   accordionItemHeader.addEventListener('click', (event) => {
@@ -22,8 +24,13 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     accordionItemHeader.classList.toggle('active');
     const accordionItemBody = accordionItemHeader.nextElementSibling;
     if (accordionItemHeader.classList.contains('active')) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + height[0].scrollHeight + height[1].scrollHeight + height[2].scrollHeight + 'px';
-      console.log(height[1].scrollHeight)
+      accordionItemBody.style.maxHeight =
+        accordionItemBody.scrollHeight +
+        height[0].scrollHeight +
+        height[1].scrollHeight +
+        height[2].scrollHeight +
+        'px';
+      console.log(height[1].scrollHeight);
       // console.log(accordionItemHeader)
     } else {
       accordionItemBody.style.maxHeight = 0;
--- research-projects/research-projects.js
+++ research-projects/research-projects.js
@@ -1,25 +1,30 @@
-
-const accordionItemHeaders = document.querySelectorAll(".research-prj__accordion-item-header");
-
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-    
+const accordionItemHeaders = document.querySelectorAll(
+  '.research-prj__accordion-item-header'
+);
+const height = document.querySelectorAll(
+  '.research-prj__accordion-item-body.research-prj__accordion-item-body_type_blue'
+);
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener('click', (event) => {
     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
+
     // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
     // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
     //   currentlyActiveAccordionItemHeader.classList.toggle("active");
     //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
     // }
 
-    accordionItemHeader.classList.toggle("active");
+    accordionItemHeader.classList.toggle('active');
     const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
+    if (accordionItemHeader.classList.contains('active')) {
+      accordionItemBody.style.maxHeight =
+        accordionItemBody.scrollHeight +
+        height[0].scrollHeight +
+        height[1].scrollHeight +
+        'px';
+        console.log(height[1].scrollHeight)
+    } else {
       accordionItemBody.style.maxHeight = 0;
     }
-    
   });
 });
